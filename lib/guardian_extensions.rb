2c4ba506433202a4f73e4a1a312d7762ce44a8e0
Martin Brennan <martin@discourse.org>
2024-01-23 11:45:44 +1000

    DEV: Change saved_searches_min_trust_level to group setting (#68)
    New setting is saved_searches_allowed_groups
    
    c.f. https://meta.discourse.org/t/we-are-changing-giving-access-to-features/283408

--- lib/guardian_extensions.rb
+++ lib/guardian_extensions.rb
@@ -3,6 +3,6 @@
 module SavedSearches::GuardianExtensions
   def can_use_saved_searches?
     SiteSetting.saved_searches_enabled? && authenticated? &&
-      (is_staff? || user.has_trust_level?(SiteSetting.saved_searches_min_trust_level))
+      (is_staff? || user.in_any_groups?(SiteSetting.saved_searches_allowed_groups_map))
   end
 end
